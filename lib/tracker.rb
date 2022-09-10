ab4f233c352060a8bf3685b72aa817525f959466
Ilia Lobsanov <ilia@nurey.com>
2022-09-10 11:18:47 -0400

    Refactor and add mac os notifications

---
+++ lib/tracker.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class Tracker
+  USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+  REFURB_URL = 'https://www.apple.com/ca/shop/refurbished/iphone'
+  THRESHOLD_PRICE = 700
+
+  def initialize(logger:)
+    @browser = Ferrum::Browser.new
+    @browser.headers.set('User-Agent' => USER_AGENT)
+    @logger = logger
+  end
+
+  def notify(&block)
+    @notifier = block
+  end
+
+  def run
+    @browser.go_to(REFURB_URL)
+    @logger.info("Browser received status #{@browser.network.status}")
+    prices = @browser.css('.as-price-currentprice')
+      .map { |node| node.text.strip.gsub(/[$,]/, '').to_i }
+
+    @logger.info("Parsed prices: #{prices}")
+
+    low_prices = prices.select { |price| price < THRESHOLD_PRICE }
+
+    @notifier.call("Found items below $#{THRESHOLD_PRICE}", REFURB_URL) if low_prices.any?
+
+    @browser.reset
+  end
+end
--- track.rb
+++ track.rb
@@ -1,16 +1,25 @@
 # frozen_string_literal: true
 
 require 'ferrum'
+require 'logger'
+require 'terminal-notifier'
 
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
-REFURB_URL = 'https://www.apple.com/ca/shop/refurbished/iphone'
-THRESHOLD_PRICE = 700
+# project-specific dependencies
+require 'tracker'
 
-browser = Ferrum::Browser.new
-browser.headers.set('User-Agent' => USER_AGENT)
-browser.go_to(REFURB_URL)
-low_prices = browser.css('.as-price-currentprice')
-  .map { |node| node.text.strip.gsub(/[$,]/, '').to_i }
-  .select { |price| price < THRESHOLD_PRICE }
+logger = Logger.new('track.log')
+logger.level = Logger::INFO
 
-puts low_prices
+tracker = Tracker.new(logger: logger)
+tracker.notify do |message, url|
+  TerminalNotifier.notify(message,
+    title: 'Refurb Tracker',
+    open: url,
+    ignoreDnD: true,
+    group: Dir.pwd)
+end
+
+loop do
+  tracker.run
+  sleep 60
+end
